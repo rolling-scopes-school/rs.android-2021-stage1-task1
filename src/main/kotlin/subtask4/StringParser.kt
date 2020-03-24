1931d8180442dae486ddd4b0d05e32132a5c1902
PodrezenkoMA <podrezenkoma@okbtsp.com>
2020-03-24 18:06:12 +0300

    feat: first implementation

--- src/main/kotlin/subtask4/StringParser.kt
+++ src/main/kotlin/subtask4/StringParser.kt
@@ -2,8 +2,37 @@ package subtask4
 
 class StringParser {
 
-    // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        var resutlStr = arrayOf<String>()
+
+        val brackets = mapOf("[" to "]", "(" to ")", "<" to ">")
+
+        inputString.forEachIndexed { index, c ->
+            val openBracket = c.toString()
+
+            if (brackets.containsKey(openBracket)) {
+                val closeBracket = brackets.getValue(openBracket)
+
+                var startIndex = index + 1
+                var endIndex = startIndex
+
+                while(endIndex >= 0) {
+                    endIndex = inputString.indexOf(closeBracket, startIndex)
+                    if (endIndex != -1) {
+                        val string = inputString.substring(index + 1, endIndex)
+                        if (string.count{ c -> c.toString() == openBracket } != string.count{ c -> c.toString() == closeBracket}) {
+                            startIndex = endIndex + 1
+                            continue
+                        }
+                        println(string)
+                        resutlStr += string
+                        break
+                    }
+                }
+
+            }
+        }
+
+        return resutlStr
     }
 }
