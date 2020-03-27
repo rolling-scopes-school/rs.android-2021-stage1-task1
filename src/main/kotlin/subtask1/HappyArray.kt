ebae4f5f3ab76e487c410935e4f0d0bccd240ba5
PodrezenkoMA <podrezenkoma@okbtsp.com>
2020-03-27 11:22:38 +0300

    feat: implement subtask1

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -2,8 +2,26 @@ package subtask1
 
 class HappyArray {
 
-    // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var resultArray = intArrayOf()
+        if (sadArray.isEmpty() || (sadArray.count() < 3)) {
+            resultArray = sadArray
+        }
+
+        resultArray = sadArray
+        var arr1: IntArray
+        do {
+            arr1 = resultArray
+            resultArray = resultArray.filterIndexed { index, value ->
+                if ((index == 0) || (index == resultArray.count() - 1)) {
+                    true
+                } else {
+                    value <= (resultArray.get(index - 1) + resultArray.get(index + 1))
+                }
+            }.toIntArray()
+
+        } while (arr1.count() != resultArray.count())
+
+        return resultArray
     }
 }
