0f91bb628da64cfd1fabab17852a71af39f2c0d5
Samakus <eveeater08@gmail.com>
2021-04-20 14:54:48 +0300

    docs: done task1

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -1,9 +1,32 @@
 package subtask1
 
 class HappyArray {
-
     // TODO: Complete the following function
+    fun isHappy(Array: IntArray) : Boolean{
+        for(i in 1..Array.size-2) {
+            if (Array[i] > (Array[i - 1] + Array[i + 1]))
+                return false
+        }
+        return true
+    }
+    fun makeHappy(sadArray: IntArray): IntArray{
+        var temp = IntArray(sadArray.size)
+        temp[0] = sadArray[0]
+        var offset = 0
+        for(i in 1..sadArray.size-2)
+        {
+            if(sadArray[i] <= (sadArray[i-1]+sadArray[i+1]))
+                temp[i-offset] = sadArray[i]
+            else
+                offset++
+        }
+        temp[sadArray.size-offset-1] = sadArray[sadArray.size-1]
+        return (temp.filter{it>0}).toIntArray()
+    }
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+    var tmp = sadArray
+        while(!isHappy(tmp))
+            tmp = makeHappy(tmp)
+        return tmp
     }
 }
