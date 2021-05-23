0c5a9c6f1d2a797e6524bb1ccb2011b165bbcd1f
Bondidos <gglbond@gmail.com>
2021-05-23 13:26:09 +0300

    all tasks1

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -2,8 +2,33 @@ package subtask3
 
 class StringParser {
 
-    // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+     val arr= mutableListOf<String>()
+
+        for (i in inputString.indices){
+            if(inputString[i]=='<' || inputString[i]=='(' || inputString[i]=='['){
+                arr.add(inputString.substring(i+1, findNextScopeIndex(inputString,i+1,inputString[i])))
+            }
+        }
+        return arr.toTypedArray()
     }
+}
+fun findNextScopeIndex(str: String, startIndex: Int, ch : Char) : Int{
+    var toFind =' '
+    var flag =0
+    when (ch){
+        '<' -> toFind ='>'
+        '[' -> toFind = ']'
+        '(' -> toFind = ')'
+    }
+    for (i in startIndex .. str.length){
+        when (str[i]){
+            ch      -> flag++
+            toFind  -> {
+                if(flag!=0) flag-- else
+                return i
+            }
+        }
+    }
+    return 0
 }
