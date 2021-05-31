ab99ac828dd7e01accabeb9f2e07f700795c21ef
Polina Yukhnevich <57799907+PolinaYu@users.noreply.github.com>
2021-05-31 23:40:27 +0300

    Update HappyArray.kt

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -3,18 +3,29 @@ package subtask1
 class HappyArray {
 
     fun convertToHappy(sadArray: IntArray): IntArray {
-        val happyArrayList = sadArray.toCollection(ArrayList())
-        val i = 1
-        while (i < happyArrayList.size - 1){
-            if((happyArrayList[i-1] + happyArrayList[i+1]) < happyArrayList[i]){
-                println(i)
-                happyArrayList.removeAt(i)
-                if(i!=1) i--
+        throw NotImplementedError("Not implemented")
+        fun convertToHappy(sadArray: IntArray): IntArray {
+
+            val happyArray = sadArray.toMutableList()
+            recursion(happyArray)
+            return happyArray.toIntArray()
+        }
+    }
+    fun recursion(array :MutableList<Int>){
+        for (i in array.indices){
+            if(i == array.indices.first)
+                continue
+            if(i==array.indices.last)
+                break
+
+            else if(array[i-1]+array[i+1]<array[i]) {
+                array.removeAt(i)
+                recursion(array)
+                break
             }
-            else{
-                i++
+            else {
+                continue
             }
-            return happyArrayList.toIntArray()
         }
     }
 }
